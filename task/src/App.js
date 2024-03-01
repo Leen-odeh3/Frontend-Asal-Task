558d594c489f7d3bc75014d00fc9a10a62e58af6
Leen-odeh3 <123558998+Leen-odeh3@users.noreply.github.com>
2024-03-02 00:09:17 +0300

    Update App.js

--- task/src/App.js
+++ task/src/App.js
@@ -1,8 +1,23 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import LeftSide from "./Component/LeftSide/LeftSide";
+import RightSide from "./Component/RightSide/RightSide";
 
-function App() {
+const App = () => {
   return (
-<>Hello</>
+    <Grid
+      container
+      spacing={3}
+      style={{ backgroundColor: "#F5F5F5", padding: "25px" }}
+    >
+      <Grid item xs={5}>
+        <LeftSide />
+      </Grid>
+      <Grid item xs={7}>
+        <RightSide />
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default App;
